8487b364b67e42b0131937c96850fafc95fe24d7
Arkadiusz <alenart1160@gmail.com>
2024-02-07 17:56:04 +0100

    test

--- math.js
+++ math.js
@@ -5,7 +5,7 @@ const fahrenheitToCelsius = (temp) => {
 const celsiusToFahrenheit = (temp) => {
 	return temp * 1.8 + 32
 }
-
+let test = fahrenheitToCelsius(32)
 module.exports = {
 	fahrenheitToCelsius,
 	celsiusToFahrenheit,
